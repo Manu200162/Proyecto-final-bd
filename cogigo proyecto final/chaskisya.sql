bc93b136a5572793c8f1b354755a77d9188c681c
HP <fpedronicolas@gmail.com>
2020-03-23 11:05:07 -0400

    creando la base de datos

--- cogigo proyecto final/chaskisya.sql
+++ cogigo proyecto final/chaskisya.sql
@@ -14,10 +14,12 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `administracion`
 --
+show databases;
+create database chaskisya;
+use chaskisya;
 
 DROP TABLE IF EXISTS `administracion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
