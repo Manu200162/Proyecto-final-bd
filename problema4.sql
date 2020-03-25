b18d1d761a7bed9e8eafea73b1833ac49d7bc4ed
Manu200162 <manli200134@gmail.com>
2020-03-24 22:23:20 -0400

    proyecto

--- problema4.sql
+++ problema4.sql
@@ -6,4 +6,10 @@ inner join ciudad on cliente.Id_ciudad = ciudad.Id_ciudad;
 -- buscamos los chaskis que tenemos al rededor del mundo
 select chaski.nombre, ciudad.nombre as ciudad,pais from ciudad
 inner join controlador on controlador.Id_ciudad = ciudad.Id_ciudad
-inner join chaski on chaski.Id_controlador = controlador.Id_controlador
+inner join chaski on chaski.Id_controlador = controlador.Id_controlador;
+
+-- Seleccionamos los nombres de los empleados que sean de América
+Select cliente.nombre, apellido  from cliente
+inner join ciudad on cliente.Id_ciudad = ciudad.Id_ciudad
+where ciudad.pais = 'Bolivia' or ciudad.pais = 'Argentina' or ciudad.pais = 'Chile' 
+or ciudad.pais = 'Estados Unidos' or ciudad.pais = 'Mexico' or ciudad.pais = 'Peru';
